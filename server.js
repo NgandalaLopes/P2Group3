a26025fc10f044fa0703223b3ab2cdad990f1f96
UrEmotional <junaidsattar47@gmail.com>
2023-03-15 19:13:54 -0500

    router fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-const routes = require('./controllers');
+const router = express.Router();
 
 const sequelize = require('./config/connection');
 require('dotenv').config();
