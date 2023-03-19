c0d89a52bb4bf97475a250ca4e22b7fb7d12d0ed
Nicole Marie <nicolependarvis5@gmail.com>
2023-03-19 11:28:32 -0400

    add Comment to const to require file

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const sequelize = require('../../config/connection');
-const {Post, User} = require('../../models');
+const {Post, User, Comment} = require('../../models');
 
 
 router.get('/', async (req, res) => {
