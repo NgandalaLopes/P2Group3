b486504955ac5fde85415e2b5f15479dd13d23f8
UrEmotional <junaidsattar47@gmail.com>
2023-03-19 21:05:52 -0500

    html to handlebar

--- controllers/api/app.js
+++ controllers/api/app.js
@@ -1,33 +1,39 @@
 const express = require('express');
 const router = express.Router();
-const sequelize = require('../config/connection');
-const {Post, User,Comment }= require('../../models');
 const http = require('http');
-
-router.get('/search/:type/:query', (req, res) => {
-    const { type, query } = req.params;
-    const url = `http://api.jikan.moe/v3/search/${type}?q=${encodeURIComponent(query)}`;
+router.get('/search/:query', async (req, res) => {
+    
+    
+    const url = `https://api.jikan.moe/v4/anime?q=${query}&sfw`;
+    const fetchAnime = async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    };
+});
+    
   
-    http.get(url, (apiRes) => {
-      let data = '';
+//     http.get(url, (apiRes) => {
+//       let data = '';
   
-      apiRes.on('data', (chunk) => {
-        data += chunk;
-      });
+//       apiRes.on('data', (chunk) => {
+//         data += chunk;
+//       });
   
-      apiRes.on('end', () => {
-        try {
-          const searchResults = JSON.parse(data);
-          res.status(200).json(searchResults);
-        } catch (error) {
-          console.error(error);
-          res.status(500).json({ message: 'Something went wrong!' });
-        }
-      });
-    }).on('error', (error) => {
-      console.error(error);
-      res.status(500).json({ message: 'Something went wrong!' });
-    });
-  });
+//       apiRes.on('end', () => {
+//         try {
+//           const searchResults = JSON.parse(data);
+//           res.status(200).json(searchResults);
+//         } catch (error) {
+//           console.error(error);
+//           res.status(500).json({ message: 'Something went wrong!' });
+//         }
+//       });
+//     }).on('error', (error) => {
+//       console.error(error);
+//       res.status(500).json({ message: 'Something went wrong!' });
+//     });
+//   });
+ 
   
   module.exports = router;
