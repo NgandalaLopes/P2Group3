1fcc975f57ee4f36d0ba6eb87941b141ae0beff6
UrEmotional <junaidsattar47@gmail.com>
2023-03-21 17:39:22 -0500

    rework

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRoutes');
-const commentRoutes = require('./commentRoutes');
 
 
 // unsure what if need to add other route files
--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -4,30 +4,5 @@ const sequelize = require('../config/connection');
 const {Post, User,Comment }= require('../../models');
 
 
-
-router.get('/', async (req, res) => {
-    const postData = await Post.findAll().catch((err) => {
-        res.json(err);
-    });
-        const posts = postData.map((post) => post.get({plain:true}));
-        res.render('all',{ posts });
-
-    });
-
- 
-// route to get a single post
-router.get('/post/:id', async (req, res) => {
-        try{ 
-            const postData = await Post.findByPk(req.params.id);
-            if(!postData) {
-                res.status(404).json({message: 'No Post exist'});
-                return;
-            }
-            const post = postData.get({ plain: true });
-            res.render('post', post);
-          } catch (err) {
-              res.status(500).json(err);
-          };     
-      });
     
     module.exports = router;
